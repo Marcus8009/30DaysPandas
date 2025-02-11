72d9bfabc53357a14e7492499c99abb5e6cd7b8d
Marcus Vos <marcusvos93@gmail.com>
2025-02-11 21:10:49 +0800

    Update Article views 1.py

--- Data Filtering/Article views 1.py
+++ Data Filtering/Article views 1.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    df=views[views['author_id']==views['viewer_id']]
-    udf=df['author_id'].unique()
-    udf=sorted(udf)
-    result=pd.DataFrame({'id': udf})
+    filtered = views[views['author_id'] == views['viewer_id']]
+    filtered_unique = filtered['author_id'].unique()
+    unique_authors = sorted(filtered_unique)
+    result = pd.DataFrame({'id' : unique_authors}) 
     return result
